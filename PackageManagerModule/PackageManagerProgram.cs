f4b4830239da7e630cf631020d04431d1c99de7c
Константин <eckumoc@gmail.com>
2022-09-04 06:17:35 +0300

    ╨б╨╛╨╖╨┤╨░╤О ╨╜╨╛╨▓╤Л╨╣ ╨┐╤А╨╛╨╡╨║╤В

---
+++ PackageManagerModule/PackageManagerProgram.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+    
+public class PackageManagerProgram
+{
+    private const string DefaultsPackageStoreDirectory = @"D:\System-Config\PackageStore";
+
+    public static void Test() 
+        => Run(new string[] { 
+            @"d:\Commands", 
+            @"d:\Commands.zip" 
+        });
+        
+
+    /// <summary>
+    /// Выполнение с параметрами
+    /// </summary>    
+    public static void Run(params string[] args)
+    {        
+        if (args.Length == 0)
+        {
+            var manager = new PackageManager(DefaultsPackageStoreDirectory);
+
+            
+            string directory = System.IO.Directory.GetCurrentDirectory();
+            Console.WriteLine("Создание пакета: "+directory);
+
+            var path = new List<string>(directory.Split("\\"));
+            string application = path.Last();
+            string version = manager.GetNextVersion(application); 
+            manager.Pack(application, version, directory);
+        }
+        else
+        {
+            var manager = new PackageManager(DefaultsPackageStoreDirectory);
+            string directory = System.IO.Directory.GetCurrentDirectory();
+            foreach (string application in args)
+            {                          
+                string version = manager.GetLastVersion(application);
+                manager.Pack(application, version, directory);
+            }
+        }
+        
+    }
+
+    
+}
+ 
